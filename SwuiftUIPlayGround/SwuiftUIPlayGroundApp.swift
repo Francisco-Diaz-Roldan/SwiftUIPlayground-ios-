03b8313881c78ae7d5812f88d743d6a16d885634
Francisco Diaz Roldan <francisco.diazro@viewnet.com>
2024-03-19 06:18:59 -0700

    Initial Commit

---
+++ SwuiftUIPlayGround/SwuiftUIPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwuiftUIPlayGroundApp.swift
+//  SwuiftUIPlayGround
+//
+//  Created by Francisco Díaz Roldán on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwuiftUIPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
